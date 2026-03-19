14e4947e32a68b14088e971fd4b2efd298bebed1
unknown <3215966955@qq.com>
2026-03-19 20:29:14 +0800

    ex02:调整闪烁间隔为1000ms，验证非阻塞逻辑

---
+++ ex02/lab02/lab02.ino
@@ -0,0 +1,25 @@
+/*
+ 作业 2 (ex02)
+ 功能：使用 millis() 实现 LED 以 1Hz 频率稳定闪烁
+ 频率说明：1Hz = 亮 500ms + 灭 500ms
+*/
+const int ledPin =2; // 使用板载LED引脚
+
+unsigned long previousMillis = 0; // 记录上一次闪烁的时间
+const long interval = 1000;       // 闪烁间隔 (1000毫秒)
+bool ledState = false;           // LED当前状态
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis(); // 获取当前系统运行时间
+
+  // 判断是否到达闪烁时间点
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;   // 记录当前时间
+    ledState = !ledState;             // 翻转状态
+    digitalWrite(ledPin, ledState);   // 控制LED
+  }
+}
